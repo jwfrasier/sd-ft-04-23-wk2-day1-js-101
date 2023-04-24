1cf1938f2550d7be8734e8fe804b400d7888f363
Joseph <frasier.joseph@gmail.com>
2023-04-24 12:50:02 -0500

    js 101 introduction to variables

---
+++ javascript101.js
@@ -0,0 +1,95 @@
+const city = "Tomball";
+let name = "Joe";
+let favFood = "Spaghetti";
+const age = "28343217";
+var zipcode = "77375";
+
+let jobTitle = "junior dev";
+
+const FINALBOSS = "coding interview";
+const salary = 0;
+const camelCasing = 0;
+const PascalCasing = 0;
+const snake_casing = 0;
+
+const cameron = "cameron";
+cameron[6]; // n
+const turbo = [];
+turbo.push(5);
+turbo[0]; // 5
+const CameronMcDuffie = {
+  age: 21,
+  handiness: "right",
+  setTimeout: 2.0,
+};
+CameronMcDuffie.age;
+
+const maxwell = "Maxwell";
+maxwell[6];
+const animals = [];
+animals.push(5);
+animals[0];
+let maxwellConway = {
+  is: "cool",
+  has: "lots of money",
+  needs: "nobody",
+};
+maxwellConway.is;
+
+const Justin = "Justin";
+Justin[5];
+const bubblegum = [];
+bubblegum.push(5);
+bubblegum[0];
+const justinAnzalone = {
+  firstName: "Justin",
+  lastName: "Anzalone",
+  charming: false,
+  tall: true,
+};
+justinAnzalone.charming;
+
+const Kim = "Kim";
+Kim[2];
+const blah = [];
+blah.push(5);
+blah[0];
+const kimWells = {
+  age: 23,
+  city: "Atlanta",
+};
+kimWells.age;
+
+const Dustin = "Dustin";
+Dustin[5];
+const pasta = [];
+pasta.push(5);
+pasta[0];
+const DustinBeckford = {
+  favFood: "CurryChicken",
+  age: "24",
+};
+Dustin.favFood;
+
+const jaye = "pasta";
+// jaye[whatgoeshere????]
+// const stuff = [];
+// stuff.push(5);
+// stuff[0];
+const jayeLasagna = {
+  pet: "husky",
+  likes: "coding",
+  doesntlike: "Joe",
+};
+jayeLasagna.pet;
+jayeLasagna["pet"];
+
+const dan = "Dan";
+dan[2];
+const happy = [];
+happy.push(5);
+happy[0];
+const danielBaron = {
+  pet: "cat",
+};
+danielBaron.pet;
